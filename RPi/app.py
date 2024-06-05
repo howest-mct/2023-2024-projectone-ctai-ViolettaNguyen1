a69e87250ac7f1b5ee09004d559ea50dd527f4c8
Violetta Nguyen <violetta.nguyen@student.howest.be>
2024-06-05 14:33:42 +0200

    Conencted AI to RPi (RPi side)

--- RPi/app.py
+++ RPi/app.py
@@ -1,43 +1,75 @@
+import socket
 import threading
-import queue
 import time
-# my own class for controlling LCD
 from LCD import LCD
 
-# Bluez gatt uart service (SERVER)
-from bluetooth_uart_server.bluetooth_uart_server import ble_gatt_uart_loop
+# Global vars for use in methods/threads
+client_socket = None
+server_socket = None
+server_thread = None
+shutdown_flag = threading.Event() # see: https://docs.python.org/3/library/threading.html#event-objects
 
-# extend this code so the value received via Bluetooth gets printed on the LCD
-# (maybe together with you Bluetooth device name or Bluetooth MAC?)
+def setup_socket_server():
+    global server_socket, server_thread, shutdown_flag
+    # Socket setup
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket instance
+    server_socket.bind(('0.0.0.0', 8500)) # bind on all available ip's (WiFi and LAN), on port 8500 (this can be anything between 1024 and 65535)
+    server_socket.settimeout(0.2)  # Timeout for listening, needed for loop in thread, otherwise it's blocking
+    server_socket.listen(1) # enable "listening" for requests / connections
 
-def main():
-    lcd_device = LCD()
-    lcd_device.setup()
-    i = 0
-    rx_q = queue.Queue()
-    tx_q = queue.Queue()
-    device_name = "viola_rpi_bluetooth" # TODO: replace with your own (unique) device name
-    threading.Thread(target=ble_gatt_uart_loop, args=(rx_q, tx_q, device_name), daemon=True).start()
-    prev_incoming = 0
-    while True:
+
+    # Start the server thread
+    server_thread = threading.Thread(target=accept_connections, args=(shutdown_flag,), daemon=True) # create the thread 
+                                                                               # where you wait for incoming connection
+    server_thread.start() # start the above thread
+
+def accept_connections(shutdown_flag):
+    global client_socket
+    print("Accepting connections")
+    while not shutdown_flag.is_set():  # as long as ctrl+c is not pressed
         try:
-            incoming = rx_q.get() # Wait for up to 1 second 
-            if i == 0:
-                lcd_device.display_message(f"Score:")
-                i = 1
-
-            if incoming!=prev_incoming:
-                print("Score: {}".format(incoming))
-                lcd_device.display_message(incoming, lcd_device.LCD_LINE_2)
-                time.sleep(0.2)
-                prev_incoming = incoming
-        except Exception as e:
-            pass # nothing in Q 
-
-        # if i%5 == 0: # Send some data every 5 iterations
-        #     tx_q.put("test{}".format(i))
-        # i += 1
-if __name__ == '__main__':
-    main()
+            client_socket, addr = server_socket.accept() # accept incoming requests, and return a reference to the client and it's IP
+            print("Connected by", addr)
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, shutdown_flag,)) # thread 
+            client_thread.start() # start the above thread; where we try to accept data
+        except socket.timeout: # ignore timeout errors
+            pass
 
 
+def handle_client(sock, shutdown_flag):
+    try:
+        prev_data = 0
+        lcd.display_message(message="Score")
+        lcd.display_message("0", line = lcd.LCD_LINE_2)
+        while not shutdown_flag.is_set(): # as long as ctrl+c is not pressed
+            data = sock.recv(1024) # try to receive 1024 bytes of data (maximum amount; can be less)
+            if not data: # when no data is received, try again (and shutdown flag is checked again)
+                break # go back to top
+            data = int(data.decode())
+            print("Received from client:", data) # print the received data, or do something with it
+            if prev_data != data:
+                lcd.display_message(message = f"{data}   ", line=lcd.LCD_LINE_2)
+            prev_data = data
+
+    except socket.timeout: # capture the timeouts 
+        pass
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        sock.close()
+
+
+###### MAIN PART ######
+try:
+    lcd = LCD()
+    lcd.setup()
+    setup_socket_server()
+    while True:
+        time.sleep(10)
+
+except KeyboardInterrupt:
+    print("Server shutting down")
+    shutdown_flag.set() # set the shutdown flag
+finally:
+    server_thread.join() # join the thread, so we wait for it to finish (gracefull exit)
+    server_socket.close() # make sure to close any open connections
